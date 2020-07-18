bf20e707713a79aa5d1757e935269142ab7f0938
Mariano Gardos <mariano.gardos@gmail.com>
2020-07-18 14:59:57 -0300

    Add components diagram for core module as part of the detailed design.

--- vsfridge-core/src/main/java/ar/net/mgardos/vsfridge/core/component/base/BaseSensor.java
+++ vsfridge-core/src/main/java/ar/net/mgardos/vsfridge/core/component/base/BaseSensor.java
@@ -1,6 +1,5 @@
 package ar.net.mgardos.vsfridge.core.component.base;
 
-import ar.net.mgardos.vsfridge.core.Product;
 import ar.net.mgardos.vsfridge.core.component.Sensor;
 
 public class BaseSensor<T> implements Sensor<T> {
