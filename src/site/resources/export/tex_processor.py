c1c3c79d802dda348dc14fd345c4ce595da5b779
Mariano Gardos <mariano.gardos@gmail.com>
2020-08-24 19:46:25 -0300

    Fix and enhance custom tex processor

--- src/site/resources/export/tex_processor.py
+++ src/site/resources/export/tex_processor.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python
 
-import sys
+import sys, getopt
 
-print 'Number of arguments:', len(sys.argv), 'arguments.'
-print 'Argument List:', str(sys.argv)
+def main(argv):
+   inputfile = ''
+   outputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["input=","output="])
+   except getopt.GetoptError:
+      print('test.py -i <inputfile> -o <outputfile>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print('test.py -i <inputfile> -o <outputfile>')
+         sys.exit()
+      elif opt in ("-i", "--input"):
+         inputfile = arg
+      elif opt in ("-o", "--output"):
+         outputfile = arg
+   print('Input file is {0}'.format(inputfile))
+   print('Output file is {0}'.format(outputfile))
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
