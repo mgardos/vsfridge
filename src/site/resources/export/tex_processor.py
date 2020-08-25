39990b58f18e4d670af4c355a9abf43d762d85d2
Mariano Gardos <mariano.gardos@gmail.com>
2020-08-25 01:27:38 -0300

    Improve custom text processor for includegraphics

--- src/site/resources/export/tex_processor.py
+++ src/site/resources/export/tex_processor.py
@@ -20,6 +20,27 @@ def main(argv):
          outputfile = arg
    print('Input file is {0}'.format(inputfile))
    print('Output file is {0}'.format(outputfile))
+   
+   try:
+      f = open("demofile.txt", "r")
+      for line in f:
+         if "\includegraphics" in line and ".puml" in line:
+            start_tex = line.index("\includegraphics")
+            length = len("\includegraphics")
+            end_tex = start_tex+length
+            print("start_tex: {} | end_tex: {}".format(start_tex, end_tex))
+            
+            start_fileext = line.index(".puml", end_tex)
+            length = len(".puml")
+            end_fileext = start_fileext+length
+            print("start: {} | end: {}".format(start2, end2))
+            
+            url = line[end_tex+1:end_fileext]
+            print("url: ".format(url))
+   except Exception as e:
+      print("Unable to process tex: {}".format(e))
+   finally:
+      f.close()
 
 if __name__ == "__main__":
    main(sys.argv[1:])
